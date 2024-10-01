11fc8c7ff9f1ff2029ba8beb1c856eff1cdb90ab
Malte <malte.stengard@gmail.com>
2024-10-01 08:51:27 +0200

    Art and advice done

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 function AdviceSection() {
+  const [favouriteAdvice, setFavAdv] = useState([]);
+  const [data, setData] = useState(null);
+
+  const fetchData = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then(response => response.json())
+      .then(data => setData(data.slip))
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="advice-slip">
+        {data ? (
+          <div>{data.id}, {data.advice}</div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </section>
+      <h2>Favourite Advice</h2>
+      <section className="favourite-slips-list">
+        {favouriteAdvice.map((advice, index) => (
+          <div key={index}>{advice.id}, {advice.advice}</div>
+        ))}
+      </section>
+      <button onClick={fetchData}>Get Advice</button>
+      <button onClick={() => setFavAdv([...favouriteAdvice, data])}>Save to Favourites</button>
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react"
+
 function ArtsSection() {
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <FetchData />
+      </div>
     </section>
   )
 }
 
 export default ArtsSection
+
+
+const FetchData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://boolean-uk-api-server.fly.dev/art')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.artist}</p>
+            <img src={'https://boolean-uk-api-server.fly.dev' + item.imageURL} alt={item.title} />
+            <p>{item.publicationHistory}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
